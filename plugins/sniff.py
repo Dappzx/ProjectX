99d67310055a7931a1396a3ca29a8ea86a4e98e7
Dappzx <73407566+Dappzx@users.noreply.github.com>
2021-06-06 06:16:00 +0700

    Update sniff.py
    :D

--- plugins/sniff.py
+++ plugins/sniff.py
@@ -1,4 +1,4 @@
-"Sniff CFG by Dappzx"
+"Build By Qano & Ported By Dappzx"
 import time
 from pyrogram.errors.exceptions.bad_request_400 import YouBlockedUser
 from userge import userge, Message
@@ -6,35 +6,60 @@ from userge.utils.exceptions import StopConversation
 
 
 #sniff
-@userge.on_cmd("sniff", about={
+@userge.on_cmd("sniff", about={ 
 	'header': "SNIFF",
 	'usage': "{tr}sniff [Reply ke cfg]\n"})
 	
-async def gen(message: Message):
-	"""Untuk sniff config"""
+async def sniff(message: Message):
+	"""Untuk sniff cfg"""
 	user = await userge.get_me()
 	uname = user.username
 	replied = message.reply_to_message
-	chat = "@hcdecryptor_bot"
-	await message.edit("```Wait for result... ```")
-	msgs = []
-	ERROR_MSG = "Unblok bot @hcdecryptor_bot untuk menggunakan command ini"
-	try:
-		async with userge.conversation(chat) as conv:
-			try:
-				await conv.forward_message(replied)
-			except YouBlockedUser:
-				await message.err(f"**{ERROR_MSG}**", del_in=5)
-				return 
-			msgs = await conv.get_response(timeout=30, mark_read=True)
-			if "Satisfied with the result" in msgs.text:
-				msgs = msgs.text.replace("Satisfied with the result? Follow us!","").replace("🌐 BOT CHANNEL: @hctoolschannel","").replace("🌐 BOT GROUP: @hctools","").replace("💻 SOURCE CODE: https://github.com/hctools/hcdrill-tg","")
-				msgs = f"Dec by: @{uname}\n\n{msgs}".replace("[>]","•")
-			else:
-				msgs = msgs.text
-	except StopConversation:
-		pass 
-	try:
-		await message.edit(f"{msgs}")
-	except AttributeError:
-		await message.edit(f"`Bot tidak merespon!`", del_in=5)
+	if not replied:
+		await message.err("```Reply ke cfg.```", del_in=5)
+		return 
+	if "npv2" in replied.document.file_name:
+		chat = "@npv_unlock_bot"
+		await message.edit("```Wait for result... ```")
+		msgs = []
+		ERROR_MSG = "Unblok bot @npv_unlock_bot untuk menggunakan command ini"
+		try:
+			async with userge.conversation(chat) as conv:
+				try:
+					await conv.forward_message(replied)
+				except YouBlockedUser:
+					await message.err(f"**{ERROR_MSG}**", del_in=5)
+					return 
+				msgs = await conv.get_response(timeout=30, mark_read=True)
+				result = msgs.text
+				if "VMESS" in result:
+					result = f"{result}".replace("=>","»") 
+		except StopConversation:
+			pass 
+		try:
+			await message.edit(f"{result}")
+		except AttributeError:
+			await message.edit(f"`Bot tidak merespon!`", del_in=5)
+	else:
+		chat = "@hcdecryptor_bot"
+		await message.edit("```Wait for result... ```")
+		msgs = []
+		ERROR_MSG = "Unblok bot @hcdecryptor_bot untuk menggunakan command ini"
+		try:
+			async with userge.conversation(chat) as conv:
+				try:
+					await conv.forward_message(replied)
+				except YouBlockedUser:
+					await message.err(f"**{ERROR_MSG}**", del_in=5)
+					return 
+				msgs = await conv.get_response(timeout=30, mark_read=True)
+				result = msgs.text
+				if "Satisfied with the result" in result:
+					result = result.replace("Satisfied with the result? Follow us!","").replace("🌐 BOT CHANNEL: @hctoolschannel","").replace("🌐 BOT GROUP: @hctools","").replace("💻 SOURCE CODE: https://github.com/hctools/hcdrill-tg","")
+					result = f"{result}".replace("[>]","•")
+		except StopConversation:
+			pass 
+		try:
+			await message.edit(f"{result}")
+		except AttributeError:
+			await message.edit(f"`Bot tidak merespon!`", del_in=5)
