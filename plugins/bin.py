3dfad8150098402c4e243af295fa81f627de6b7e
Dappzxㅤ <73407566+Dappzx@users.noreply.github.com>
2022-02-05 23:31:45 -0600

    Update bin.py

--- plugins/bin.py
+++ plugins/bin.py
@@ -13,14 +13,14 @@ from userge.utils.exceptions import StopConversation
 async def gen(message: Message):
 	"""Untuk gen cc"""
 	replied = message.input_str
-	chat = "@Carol5_bot"
+	chat = "@rkhackingbot"
 	await message.edit("```Wait for result... ```")
 	msgs = []
-	ERROR_MSG = "Unblok bot @Carol5_bot untuk menggunakan command ini"
+	ERROR_MSG = "Unblok bot @rkhackingbot untuk menggunakan command ini"
 	try:
 		async with userge.conversation(chat) as conv:
 			try:
-				await conv.send_message("!gen {}".format(replied))
+				await conv.send_message("/gen {}".format(replied))
 			except YouBlockedUser:
 				await message.err(f"**{ERROR_MSG}**", del_in=5)
 				return 
@@ -101,14 +101,13 @@ async def gen(message: Message):
 async def bin(message: Message):
 	"""Untuk cek bin"""
 	replied = message.input_str
-	chat = "@Carol5_bot"
-	await message.edit("```Wait for result...```")
+	chat = "```Wait for result...```")
 	msgs = []
-	ERROR_MSG = "Unblok bot @Carol5_bot untuk menggunakan command ini"
+	ERROR_MSG = "Unblok bot @rkhackingbot untuk menggunakan command ini"
 	try:
 		async with userge.conversation(chat) as conv:
 			try:
-				await conv.send_message("!bin {}".format(replied))
+				await conv.send_message("/bin {}".format(replied))
 			except YouBlockedUser:
 				await message.err(f"**{ERROR_MSG}**", del_in=5)
 				return 
@@ -182,21 +181,21 @@ async def bin(message: Message):
 
 
 #key
-@userge.on_cmd("key", about={
-	'header': "KEY",
-	'usage': "{tr}key [Enter key]\n"})
+@userge.on_cmd("sk", about={
+	'header': "SK",
+	'usage': "{tr}sk [Enter key]\n"})
 	
 async def key(message: Message):
 	"""Untuk cek sk_live key"""
 	replied = message.input_str
-	chat = "@Carol5_bot"
+	chat = "@rkhackingbot"
 	await message.edit("```Wait for result...```")
 	msgs = []
-	ERROR_MSG = "Unblok bot @Carol5_bot untuk menggunakan command ini"
+	ERROR_MSG = "Unblok bot @rkhackingbot untuk menggunakan command ini"
 	try:
 		async with userge.conversation(chat) as conv:
 			try:
-				await conv.send_message("!key {}".format(replied))
+				await conv.send_message("/sk {}".format(replied))
 			except YouBlockedUser:
 				await message.err(f"**{ERROR_MSG}**", del_in=5)
 				return 
@@ -270,21 +269,21 @@ async def key(message: Message):
 
 
 #iban
-@userge.on_cmd("iban", about={
-	'header': "IBAN",
-	'usage': "{tr}iban [Enter iban]\n"})
+@userge.on_cmd("chk", about={
+	'header': "Auth Only",
+	'usage': "{tr}chk [Enter cc]\n"})
 	
-async def iban(message: Message):
-	"""Untuk cek iban"""
+async def gen(message: Message):
+	"""Untuk cek cc"""
 	replied = message.input_str
-	chat = "@Carol5_bot"
+	chat = "@rkhackingbot"
 	await message.edit("```Wait for result...```")
 	msgs = []
-	ERROR_MSG = "Unblok bot @Carol5_bot untuk menggunakan command ini"
+	ERROR_MSG = "Unblok bot @rkhackingbot untuk menggunakan command ini"
 	try:
 		async with userge.conversation(chat) as conv:
 			try:
-				await conv.send_message("!iban {}".format(replied))
+				await conv.send_message("/chk {}".format(replied))
 			except YouBlockedUser:
 				await message.err(f"**{ERROR_MSG}**", del_in=5)
 				return 
@@ -365,17 +364,14 @@ async def iban(message: Message):
 async def vbv(message: Message):
 	"""Untuk cek vbv cc"""
 	replied = message.input_str
-	chat = "@Carol5_bot"
+	chat = "@rkhackingbot"
 	await message.edit("```Wait for result...```")
 	msgs = []
-	ERROR_MSG = "Unblok bot @Carol5_bot untuk menggunakan command ini"
-	if str(replied[:1]) in ['3','6']:
-		await message.err("```Hanya bisa cek VISA & MC```", del_in=5) 
-		return 
+	ERROR_MSG = "Unblok bot @rkhackingbot untuk menggunakan command ini"
 	try:
 		async with userge.conversation(chat) as conv:
 			try:
-				await conv.send_message("!vbv {}".format(replied))
+				await conv.send_message("/vbv {}".format(replied))
 			except YouBlockedUser:
 				await message.err(f"**{ERROR_MSG}**", del_in=5)
 				return 
@@ -449,21 +445,21 @@ async def vbv(message: Message):
 				
 
 #pp
-@userge.on_cmd("pp", about={
-	'header': "PP",
-	'usage': "{tr}pp [Enter full card in format]\n"})
+@userge.on_cmd("fake", about={
+	'header': "Random Address",
+	'usage': "{tr}fake [Enter Country Code]\n"})
 	
 async def pp(message: Message):
-	"""Untuk cek PayPal cc"""
+	"""Untuk Gen Random Address"""
 	replied = message.input_str
-	chat = "@Carol5_bot"
+	chat = "@rkhackingbot"
 	await message.edit("```Wait for result...```")
 	msgs = []
-	ERROR_MSG = "Unblok bot @Carol5_bot untuk menggunakan command ini"
+	ERROR_MSG = "Unblok bot @rkhackingbot untuk menggunakan command ini"
 	try:
 		async with userge.conversation(chat) as conv:
 			try:
-				await conv.send_message("!pp {}".format(replied))
+				await conv.send_message("/fake {}".format(replied))
 			except YouBlockedUser:
 				await message.err(f"**{ERROR_MSG}**", del_in=5)
 				return 
@@ -535,356 +531,3 @@ async def pp(message: Message):
 	except AttributeError:
 		await message.edit(f"`Bot tidak merespon!`", del_in=5)
 
-
-#au
-@userge.on_cmd("au", about={
-	'header': "AU",
-	'usage': "{tr}au [Enter full card in format]\n"})
-	
-async def au(message: Message):
-	"""Untuk cek stripe auth cc"""
-	replied = message.input_str
-	chat = "@Carol5_bot"
-	await message.edit("```Wait for result...```")
-	msgs = []
-	ERROR_MSG = "Unblok bot @Carol5_bot untuk menggunakan command ini"
-	try:
-		async with userge.conversation(chat) as conv:
-			try:
-				await conv.send_message("!au {}".format(replied))
-			except YouBlockedUser:
-				await message.err(f"**{ERROR_MSG}**", del_in=5)
-				return 
-			msgs = await conv.get_response(timeout=30, mark_read=True)
-			if "Wait" in msgs.text:
-				time.sleep(1)
-				msgs = await conv.get_response(timeout=30, mark_read=True) 
-				if "Wait" in msgs.text:
-					time.sleep(1)
-					msgs = await conv.get_response(timeout=30, mark_read=True) 
-					if "Wait" in msgs.text:
-						time.sleep(1)
-						msgs = await conv.get_response(timeout=30, mark_read=True) 
-						if "Wait" in msgs.text:
-							time.sleep(1)
-							msgs = await conv.get_response(timeout=30, mark_read=True)
-							if "Wait" in msgs.text:
-								time.sleep(1)
-								msgs = await conv.get_response(timeout=30, mark_read=True)
-								if "Wait" in msgs.text:
-									time.sleep(1)
-									msgs = await conv.get_response(timeout=30, mark_read=True)
-									if "Wait" in msgs.text:
-										time.sleep(1)
-										msgs = await conv.get_response(timeout=30, mark_read=True)
-										if "Wait" in msgs.text:
-											time.sleep(1)
-											msgs = await conv.get_response(timeout=30, mark_read=True)
-											if "Wait" in msgs.text:
-												time.sleep(1)
-												msgs = await conv.get_response(timeout=30, mark_read=True)
-												if "Wait" in msgs.text:
-													time.sleep(1)
-													msgs = await conv.get_response(timeout=30, mark_read=True)
-													if "Wait" in msgs.text:
-														time.sleep(1)
-														msgs = await conv.get_response(timeout=30, mark_read=True) 
-														if "Wait" in msgs.text:
-															time.sleep(1)
-															msgs = await conv.get_response(timeout=30, mark_read=True)  
-															if "Wait" in msgs.text:
-																time.sleep(1)
-																msgs = await conv.get_response(timeout=30, mark_read=True)  
-																if "Wait" in msgs.text:
-																	time.sleep(1)
-																	msgs = await conv.get_response(timeout=30, mark_read=True) 
-																	if "Wait" in msgs.text:
-																		time.sleep(1)
-																		msgs = await conv.get_response(timeout=30, mark_read=True) 
-																		if "Wait" in msgs.text:
-																			time.sleep(1)
-																			msgs = await conv.get_response(timeout=30, mark_read=True) 
-																			if "Wait" in msgs.text:
-																				time.sleep(1)
-																				msgs = await conv.get_response(timeout=30, mark_read=True) 
-																				if "Wait" in msgs.text:
-																					time.sleep(1)
-																					msgs = await conv.get_response(timeout=30, mark_read=True) 
-																					if "Wait" in msgs.text:
-																						time.sleep(1)
-																						msgs = await conv.get_response(timeout=30, mark_read=True) 
-																						if "Wait" in msgs.text:
-																							time.sleep(1)
-																							msgs = await conv.get_response(timeout=30, mark_read=True) 
-	except StopConversation:
-		pass 
-	try:
-		await message.edit(f"{msgs.text}")
-	except AttributeError:
-		await message.edit(f"`Bot tidak merespon!`", del_in=5)
-
-
-#ch
-@userge.on_cmd("ch", about={
-	'header': "CH",
-	'usage': "{tr}ch  [Enter full card in format]\n"})
-	
-async def ch(message: Message):
-	"""Untuk cek ch auth cc"""
-	replied = message.input_str
-	chat = "@Carol5_bot"
-	await message.edit("```Wait for result...```")
-	msgs = []
-	ERROR_MSG = "Unblok bot @Carol5_bot untuk menggunakan command ini"
-	try:
-		async with userge.conversation(chat) as conv:
-			try:
-				await conv.send_message("!ch {}".format(replied))
-			except YouBlockedUser:
-				await message.err(f"**{ERROR_MSG}**", del_in=5)
-				return 
-			msgs = await conv.get_response(timeout=30, mark_read=True)
-			if "Wait" in msgs.text:
-				time.sleep(1)
-				msgs = await conv.get_response(timeout=30, mark_read=True) 
-				if "Wait" in msgs.text:
-					time.sleep(1)
-					msgs = await conv.get_response(timeout=30, mark_read=True) 
-					if "Wait" in msgs.text:
-						time.sleep(1)
-						msgs = await conv.get_response(timeout=30, mark_read=True) 
-						if "Wait" in msgs.text:
-							time.sleep(1)
-							msgs = await conv.get_response(timeout=30, mark_read=True)
-							if "Wait" in msgs.text:
-								time.sleep(1)
-								msgs = await conv.get_response(timeout=30, mark_read=True)
-								if "Wait" in msgs.text:
-									time.sleep(1)
-									msgs = await conv.get_response(timeout=30, mark_read=True)
-									if "Wait" in msgs.text:
-										time.sleep(1)
-										msgs = await conv.get_response(timeout=30, mark_read=True)
-										if "Wait" in msgs.text:
-											time.sleep(1)
-											msgs = await conv.get_response(timeout=30, mark_read=True)
-											if "Wait" in msgs.text:
-												time.sleep(1)
-												msgs = await conv.get_response(timeout=30, mark_read=True)
-												if "Wait" in msgs.text:
-													time.sleep(1)
-													msgs = await conv.get_response(timeout=30, mark_read=True)
-													if "Wait" in msgs.text:
-														time.sleep(1)
-														msgs = await conv.get_response(timeout=30, mark_read=True) 
-														if "Wait" in msgs.text:
-															time.sleep(1)
-															msgs = await conv.get_response(timeout=30, mark_read=True)  
-															if "Wait" in msgs.text:
-																time.sleep(1)
-																msgs = await conv.get_response(timeout=30, mark_read=True)  
-																if "Wait" in msgs.text:
-																	time.sleep(1)
-																	msgs = await conv.get_response(timeout=30, mark_read=True) 
-																	if "Wait" in msgs.text:
-																		time.sleep(1)
-																		msgs = await conv.get_response(timeout=30, mark_read=True) 
-																		if "Wait" in msgs.text:
-																			time.sleep(1)
-																			msgs = await conv.get_response(timeout=30, mark_read=True) 
-																			if "Wait" in msgs.text:
-																				time.sleep(1)
-																				msgs = await conv.get_response(timeout=30, mark_read=True) 
-																				if "Wait" in msgs.text:
-																					time.sleep(1)
-																					msgs = await conv.get_response(timeout=30, mark_read=True) 
-																					if "Wait" in msgs.text:
-																						time.sleep(1)
-																						msgs = await conv.get_response(timeout=30, mark_read=True) 
-																						if "Wait" in msgs.text:
-																							time.sleep(1)
-																							msgs = await conv.get_response(timeout=30, mark_read=True) 
-	except StopConversation:
-		pass 
-	try:
-		await message.edit(f"{msgs.text}")
-	except AttributeError:
-		await message.edit(f"`Bot tidak merespon!`", del_in=5)
-
-
-#ss
-@userge.on_cmd("ss", about={
-	'header': "SS",
-	'usage': "{tr}ss [Enter full card in format]\n"})
-	
-async def ss(message: Message):
-	"""Untuk cek ss auth cc"""
-	replied = message.input_str
-	chat = "@Carol5_bot"
-	await message.edit("```Wait for result...```")
-	msgs = []
-	ERROR_MSG = "Unblok bot @Carol5_bot untuk menggunakan command ini"
-	try:
-		async with userge.conversation(chat) as conv:
-			try:
-				await conv.send_message("!ss {}".format(replied))
-			except YouBlockedUser:
-				await message.err(f"**{ERROR_MSG}**", del_in=5)
-				return 
-			msgs = await conv.get_response(timeout=30, mark_read=True)
-			if "Wait" in msgs.text:
-				time.sleep(1)
-				msgs = await conv.get_response(timeout=30, mark_read=True) 
-				if "Wait" in msgs.text:
-					time.sleep(1)
-					msgs = await conv.get_response(timeout=30, mark_read=True) 
-					if "Wait" in msgs.text:
-						time.sleep(1)
-						msgs = await conv.get_response(timeout=30, mark_read=True) 
-						if "Wait" in msgs.text:
-							time.sleep(1)
-							msgs = await conv.get_response(timeout=30, mark_read=True)
-							if "Wait" in msgs.text:
-								time.sleep(1)
-								msgs = await conv.get_response(timeout=30, mark_read=True)
-								if "Wait" in msgs.text:
-									time.sleep(1)
-									msgs = await conv.get_response(timeout=30, mark_read=True)
-									if "Wait" in msgs.text:
-										time.sleep(1)
-										msgs = await conv.get_response(timeout=30, mark_read=True)
-										if "Wait" in msgs.text:
-											time.sleep(1)
-											msgs = await conv.get_response(timeout=30, mark_read=True)
-											if "Wait" in msgs.text:
-												time.sleep(1)
-												msgs = await conv.get_response(timeout=30, mark_read=True)
-												if "Wait" in msgs.text:
-													time.sleep(1)
-													msgs = await conv.get_response(timeout=30, mark_read=True)
-													if "Wait" in msgs.text:
-														time.sleep(1)
-														msgs = await conv.get_response(timeout=30, mark_read=True) 
-														if "Wait" in msgs.text:
-															time.sleep(1)
-															msgs = await conv.get_response(timeout=30, mark_read=True)  
-															if "Wait" in msgs.text:
-																time.sleep(1)
-																msgs = await conv.get_response(timeout=30, mark_read=True)  
-																if "Wait" in msgs.text:
-																	time.sleep(1)
-																	msgs = await conv.get_response(timeout=30, mark_read=True) 
-																	if "Wait" in msgs.text:
-																		time.sleep(1)
-																		msgs = await conv.get_response(timeout=30, mark_read=True) 
-																		if "Wait" in msgs.text:
-																			time.sleep(1)
-																			msgs = await conv.get_response(timeout=30, mark_read=True) 
-																			if "Wait" in msgs.text:
-																				time.sleep(1)
-																				msgs = await conv.get_response(timeout=30, mark_read=True) 
-																				if "Wait" in msgs.text:
-																					time.sleep(1)
-																					msgs = await conv.get_response(timeout=30, mark_read=True) 
-																					if "Wait" in msgs.text:
-																						time.sleep(1)
-																						msgs = await conv.get_response(timeout=30, mark_read=True) 
-																						if "Wait" in msgs.text:
-																							time.sleep(1)
-																							msgs = await conv.get_response(timeout=30, mark_read=True) 
-	except StopConversation:
-		pass 
-	try:
-		await message.edit(f"{msgs.text}")
-	except AttributeError:
-		await message.edit(f"`Bot tidak merespon!`", del_in=5)
-
-
-#st
-@userge.on_cmd("st", about={
-	'header': "ST",
-	'usage': "{tr}st [Enter full card in format]\n"})
-	
-async def st(message: Message):
-	"""Untuk cek stripe charge cc"""
-	replied = message.input_str
-	chat = "@Carol5_bot"
-	await message.edit("```Wait for result...```")
-	msgs = []
-	ERROR_MSG = "Unblok bot @Carol5_bot untuk menggunakan command ini"
-	try:
-		async with userge.conversation(chat) as conv:
-			try:
-				await conv.send_message("!st {}".format(replied))
-			except YouBlockedUser:
-				await message.err(f"**{ERROR_MSG}**", del_in=5)
-				return 
-			msgs = await conv.get_response(timeout=30, mark_read=True)
-			if "Wait" in msgs.text:
-				time.sleep(1)
-				msgs = await conv.get_response(timeout=30, mark_read=True) 
-				if "Wait" in msgs.text:
-					time.sleep(1)
-					msgs = await conv.get_response(timeout=30, mark_read=True) 
-					if "Wait" in msgs.text:
-						time.sleep(1)
-						msgs = await conv.get_response(timeout=30, mark_read=True) 
-						if "Wait" in msgs.text:
-							time.sleep(1)
-							msgs = await conv.get_response(timeout=30, mark_read=True)
-							if "Wait" in msgs.text:
-								time.sleep(1)
-								msgs = await conv.get_response(timeout=30, mark_read=True)
-								if "Wait" in msgs.text:
-									time.sleep(1)
-									msgs = await conv.get_response(timeout=30, mark_read=True)
-									if "Wait" in msgs.text:
-										time.sleep(1)
-										msgs = await conv.get_response(timeout=30, mark_read=True)
-										if "Wait" in msgs.text:
-											time.sleep(1)
-											msgs = await conv.get_response(timeout=30, mark_read=True)
-											if "Wait" in msgs.text:
-												time.sleep(1)
-												msgs = await conv.get_response(timeout=30, mark_read=True)
-												if "Wait" in msgs.text:
-													time.sleep(1)
-													msgs = await conv.get_response(timeout=30, mark_read=True)
-													if "Wait" in msgs.text:
-														time.sleep(1)
-														msgs = await conv.get_response(timeout=30, mark_read=True) 
-														if "Wait" in msgs.text:
-															time.sleep(1)
-															msgs = await conv.get_response(timeout=30, mark_read=True)  
-															if "Wait" in msgs.text:
-																time.sleep(1)
-																msgs = await conv.get_response(timeout=30, mark_read=True)  
-																if "Wait" in msgs.text:
-																	time.sleep(1)
-																	msgs = await conv.get_response(timeout=30, mark_read=True) 
-																	if "Wait" in msgs.text:
-																		time.sleep(1)
-																		msgs = await conv.get_response(timeout=30, mark_read=True) 
-																		if "Wait" in msgs.text:
-																			time.sleep(1)
-																			msgs = await conv.get_response(timeout=30, mark_read=True) 
-																			if "Wait" in msgs.text:
-																				time.sleep(1)
-																				msgs = await conv.get_response(timeout=30, mark_read=True) 
-																				if "Wait" in msgs.text:
-																					time.sleep(1)
-																					msgs = await conv.get_response(timeout=30, mark_read=True) 
-																					if "Wait" in msgs.text:
-																						time.sleep(1)
-																						msgs = await conv.get_response(timeout=30, mark_read=True) 
-																						if "Wait" in msgs.text:
-																							time.sleep(1)
-																							msgs = await conv.get_response(timeout=30, mark_read=True) 
-	except StopConversation:
-		pass 
-	try:
-		await message.edit(f"{msgs.text}")
-	except AttributeError:
-		await message.edit(f"`Bot tidak merespon!`", del_in=5)
-		
-		
